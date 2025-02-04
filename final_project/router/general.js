8c90d3f3943555fa50edd97cccbb1d1bf466cabd
Sriharshini43 <109490959+Sriharshini43@users.noreply.github.com>
2025-02-04 12:20:59 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,73 +4,70 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+  return users.some((user) => user.username === username);
+};
+
+const getAllBooks = () => {
+  return books;
+};
+
 // Register a new user
 public_users.post("/register", (req, res) => {
-    const { username, password } = req.body;
-
+    const username = req.body.username;
+    const password = req.body.password;
+  
     if (!username || !password) {
-        return res.status(400).json({ message: "Username and password are required" });
+      return res.status(404).json({ message: "Missing username or password" });
+    } else if (doesExist(username)) {
+      return res.status(404).json({ message: "user already exists." });
+    } else {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registered.  Please login." });
     }
+  });
 
-    if (users.find(user => user.username === username)) {
-        return res.status(400).json({ message: "Username already exists" });
-    }
-
-    users.push({ username, password });
-    return res.status(201).json({ message: "User registered successfully" });
-});
-
-// Get the book list available in the shop
+  // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    return res.status(200).json({ books: books });
+  return res.status(200).json({ books: books });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
+  const isbn = req.params.isbn;
+  const book = books[isbn];
 
-    if (book) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
+  if (book) {
+      return res.status(200).json(book);
+  } else {
+      return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
 
-    if (booksByAuthor.length > 0) {
-        return res.status(200).json({ books: booksByAuthor });
-    } else {
-        return res.status(404).json({ message: "No books found for this author" });
-    }
+  if (booksByAuthor.length > 0) {
+      return res.status(200).json({ books: booksByAuthor });
+  } else {
+      return res.status(404).json({ message: "No books found for this author" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    const booksByTitle = Object.values(books).filter(book => book.title === title);
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(book => book.title === title);
 
-    if (booksByTitle.length > 0) {
-        return res.status(200).json({ books: booksByTitle });
-    } else {
-        return res.status(404).json({ message: "No books found with this title" });
-    }
-});
-
-// Get book reviews based on ISBN
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if (book && book.reviews) {
-        return res.status(200).json({ reviews: book.reviews });
-    } else {
-        return res.status(404).json({ message: "No reviews found for this book" });
-    }
+  if (booksByTitle.length > 0) {
+      return res.status(200).json({ books: booksByTitle });
+  } else {
+      return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 /* Get the book list available in the shop
@@ -118,5 +115,17 @@ public_users.get("/isbn/:isbn", async (req, res) => {
       return res.status(404).json({ message: "Title not found." });
     }
   }); */
+
+// Get book reviews based on ISBN
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if (book && book.reviews) {
+        return res.status(200).json({ reviews: book.reviews });
+    } else {
+        return res.status(404).json({ message: "No reviews found for this book" });
+    }
+});
 
 module.exports.general = public_users;
